d79645901d8c90c0ad1d6ca42c66148cabcbdf8e
Ilya <ilya-shabelnik@mail.ru>
2018-11-08 22:36:39 +0200

    added function remove dialog window

--- src/templates/t1/js/index.js
+++ src/templates/t1/js/index.js
@@ -1,3 +1,5 @@
 import '../../../scripts/main'
 import './filter'
 import './qrgenerate'
+/*
+import '../../../../node_modules/qrcode-generator/qrcode'*/
--- src/templates/t1/js/qrgenerate.js
+++ src/templates/t1/js/qrgenerate.js
@@ -8,6 +8,13 @@ DOMIsLoaded((event) => {
     bntQr.onclick = () => {
         createDivForQR()
         /*createQrCode()*/
+        if (document.querySelector('#h-rs-wrap__overlay').classList) {
+            document.querySelector('#h-rs-wrap__overlay').classList.contains('active-overlay')
+            document.getElementById('h-rs-wrap__overlay').addEventListener('click', () => {
+                document.getElementById('h-rs-wrap__overlay').remove();
+                document.getElementById('h-rs__bottom__imgQrCode').remove();
+            } )
+        }
     }
 })
 
@@ -16,6 +23,7 @@ const createDivForQR = () => {
     const div2 = document.createElement('div')
 
     div.id = 'h-rs-wrap__overlay'
+    div.className = 'active-overlay'
     div2.id = 'h-rs__bottom__imgQrCode'
 
     document.getElementById('app').appendChild(div)
@@ -23,5 +31,19 @@ const createDivForQR = () => {
 }
 
 const createQrCode = () => {
+    const typeNumber = 4;
+    const errorCorrectionLevel = 'L';
+    let qr = qrcode(typeNumber, errorCorrectionLevel);
+    const textForQr = document.querySelector('#text').value
+    qr.addData(textForQr);
+    qr.make();
+    document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+}
 
+NodeList.prototype.remove = HTMLCollection.prototype.remove = () => {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
 }
