318c2f97363b7cb930a215ed009664d00c4d3d80
Ilya <ilya-shabelnik@mail.ru>
2018-11-11 16:44:03 +0200

    Code style

--- src/templates/t1/js/qrgenerate.js
+++ src/templates/t1/js/qrgenerate.js
@@ -1,10 +1,30 @@
 import {DOMIsLoaded} from "../../../scripts/utils";
 
-const bntQr = document.getElementById('BtnQrCode')
+const createDivForQR = () => {
+    const div = document.createElement('div')
+    const div2 = document.createElement('div')
+
+    div.id = 'h-rs-wrap__overlay'
+    div.className = 'active-overlay'
+    div2.id = 'h-rs__bottom__imgQrCode'
+
+    document.getElementById('app').appendChild(div)
+    document.getElementById('app').appendChild(div2)
+}
+
+NodeList.prototype.remove = HTMLCollection.prototype.remove = () => {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
 /**
  * @param event
  */
 DOMIsLoaded((event) => {
+    const bntQr = document.getElementById('BtnQrCode')
     bntQr.onclick = () => {
         createDivForQR()
         /*createQrCode()*/
@@ -18,18 +38,6 @@ DOMIsLoaded((event) => {
     }
 })
 
-const createDivForQR = () => {
-    const div = document.createElement('div')
-    const div2 = document.createElement('div')
-
-    div.id = 'h-rs-wrap__overlay'
-    div.className = 'active-overlay'
-    div2.id = 'h-rs__bottom__imgQrCode'
-
-    document.getElementById('app').appendChild(div)
-    document.getElementById('app').appendChild(div2)
-}
-
 const createQrCode = () => {
     const typeNumber = 4;
     const errorCorrectionLevel = 'L';
@@ -38,12 +46,4 @@ const createQrCode = () => {
     qr.addData(textForQr);
     qr.make();
     document.getElementById('placeHolder').innerHTML = qr.createImgTag();
-}
-
-NodeList.prototype.remove = HTMLCollection.prototype.remove = () => {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
 }
